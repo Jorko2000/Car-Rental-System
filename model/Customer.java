14e097bdf1405d1fa5b03dc52562ed5cdb329b93
Jorko2000 <george1102@abv.bg>
2026-03-28 14:40:57 +0200

    Customer.java

---
+++ model/Customer.java
@@ -0,0 +1,11 @@
+public class Customer {
+    private String id;
+    private String name;
+
+    public Customer(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Getters
+}
